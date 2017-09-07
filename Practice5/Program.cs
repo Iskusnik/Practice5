885462f3f50a3daf50c8f34a816352250a31b0fb
IskusnikXD <IskusnikXD@ISKUSNIK>
2017-09-07 21:19:13 +0500

    Дополнительные возможности определения массива

--- Practice5/Program.cs
+++ Practice5/Program.cs
@@ -10,52 +10,46 @@ namespace Practice5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1 - Ввод своих элементов - действительных чисел в порядке от a1 до a64");//Не обязательно
-            Console.WriteLine("2 - Заполнение элементов соответсвенно номеру а1 = 1, а2 = 2, а3 = 3 и тд");
 
             //А такое заполнение:
             //а1  а3  а4  а10   
             //а2  а5  а9  а11
             //а6  а8  а12 а15
-            //а7  а13 а14 а16                                 - обязательно
+            //а7  а13 а14 а16        
+            Console.WriteLine("Введите сторону квадрата, который будет заполнен числами");
             int N = int.Parse(Console.ReadLine());
             int[,] arr = new int[N, N];
             int x = 0, y = 0;
             int i = 1;
-
-            arr[0, 0] = 1;
-            i++;
-            while (i <= N * N) //x != N - 1 && y != N - 1
-            {
-                
-
-
-                /*
-                if (x == N - 1 || x == 0)
+            int[] a = new int[N * N];
+            
+            Console.WriteLine("Введите 0 для ручного заполнения последовательности a");
+            Console.WriteLine("Введите не 0 для заполнения последовательности a по типу a1 = 1, a2 = 2 ... an = n");
+            if (Console.ReadLine() == "0")
+                for (i = 0; i < N * N; i++)
                 {
-                    y++;
-                    arr[y, x] = i;
-                    i++;
+                    Console.WriteLine("a{0} = ", i + 1);
+                    a[i] = int.Parse(Console.ReadLine());
                 }
-                else
-                if (y == N - 1 || y == 0)
-                {
-                    x++;
-                    arr[y, x] = i;
-                    i++;
-                }*/
+            else
+                for (i = 0; i < N * N; i++)
+                    a[i] = i + 1;
+            arr[0, 0] = a[0];
+            i = 1;
+            while (i < N * N) //x != N - 1 && y != N - 1
+            {
 
                 if (x == 0 && y != N - 1)
                 {
                     y++;
-                    arr[y, x] = i;
+                    arr[y, x] = a[i];
                     i++;
 
                     while (y != 0)
                     {
                         y--;
                         x++;
-                        arr[y, x] = i;
+                        arr[y, x] = a[i];
                         i++;
                     }
                 }
@@ -63,14 +57,14 @@ namespace Practice5
                 if (y == N - 1 && x != N - 1)
                 {
                     x++;
-                    arr[y, x] = i;
+                    arr[y, x] = a[i];
                     i++;
 
                     while (x != N - 1)
                     {
                         y--;
                         x++;
-                        arr[y, x] = i;
+                        arr[y, x] = a[i];
                         i++;
                     }
                 }
@@ -78,14 +72,14 @@ namespace Practice5
                 if (y == 0 && x != N - 1)
                 {
                     x++;
-                    arr[y, x] = i;
+                    arr[y, x] = a[i];
                     i++;
 
                     while (x != 0)
                     {
                         y++;
                         x--;
-                        arr[y, x] = i;
+                        arr[y, x] = a[i];
                         i++;
                     }
                 }
@@ -93,21 +87,16 @@ namespace Practice5
                 if (x == N - 1 && y != N - 1)
                 {
                     y++;
-                    arr[y, x] = i;
+                    arr[y, x] = a[i];
                     i++;
                     while (y != N - 1)
                     {
                         y++;
                         x--;
-                        arr[y, x] = i;
+                        arr[y, x] = a[i];
                         i++;
                     }
                 }
-
-                
-                
-                
-
             }
 
             for (int Ycoord = 0; Ycoord < N; Ycoord++)
@@ -117,7 +106,6 @@ namespace Practice5
                 for (int Xcoord = 0; Xcoord < N; Xcoord++)
                     Console.Write("{0,-5}", arr[Ycoord, Xcoord]);
             }
-            // Движение по диагонале до стенки, шаг вдоль стенки
         }
     }
 }
